593e2bc04972ba331c7d087b9c5c3fc441cef624
taji <taji@sonicgarden.jp>
2026-01-24 23:48:01 +0900

    testデータ書き換え

--- src/StudyLog.jsx
+++ src/StudyLog.jsx
@@ -1,11 +1,7 @@
 import { useState } from "react";
 
 export const StudyLogs = () => {
-  const records = [
-    { title: "勉強の記録1", time: 1},
-    { title: "勉強の記録2", time: 3},
-    { title: "勉強の記録3", time: 5}
-  ];
+  const [records, setRecords] = useState([]);
   const [studyContent, setStudyContent] = useState("");
   const [studyTime, setStudyTime] = useState("");
   const onChangeStudyContent = (e) => {
