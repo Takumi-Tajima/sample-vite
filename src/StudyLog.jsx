81835ffe6ce431abfb98ac70f1adc5e8ae5d024b
taji <taji@sonicgarden.jp>
2026-01-24 23:33:21 +0900

    入力できるように

--- src/StudyLog.jsx
+++ src/StudyLog.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
+
 export const StudyLogs = () => {
   const records = [
     { title: "勉強の記録1", time: 1},
     { title: "勉強の記録2", time: 3},
     { title: "勉強の記録3", time: 5}
-]
+  ];
+  const [studyContent, setStudyContent] = useState("");
+  const [studyTime, setStudyTime] = useState("");
+  const onChangeStudyContent = (e) => {
+    setStudyContent(e.target.value);
+  };
+  const onChangeStudyTime = (e) => {
+    setStudyTime(e.target.value);
+  };
+
   return (
     <>
       <h1>学習記録一覧</h1>
+      <div>
+        <label htmlFor="studyContent">学習内容</label>
+        <input id="studyContent" value={studyContent} onChange={onChangeStudyContent} />
+      </div>
+      <div>
+        <label htmlFor="studyTime">学習時間</label>
+        <input id="studyTime" value={studyTime} onChange={onChangeStudyTime} />時間
+      </div>
+      <p>入力されている学習内容: {studyContent}</p>
+      <p>入力されている学習時間: {studyTime}時間</p>
       <ul>
         {records.map((record, index) => (
           <li key={index}>{record.title} {record.time}時間</li>
