93e5ab3c3eab5577c2bd70a9132e06992062e396
taji <taji@sonicgarden.jp>
2026-01-24 23:22:32 +0900

    学習一覧を表示

--- src/StudyLog.jsx
+++ src/StudyLog.jsx
@@ -1,7 +1,17 @@
 export const StudyLogs = () => {
+  const records = [
+    { title: "勉強の記録1", time: 1},
+    { title: "勉強の記録2", time: 3},
+    { title: "勉強の記録3", time: 5}
+]
   return (
     <>
       <h1>学習記録一覧</h1>
+      <ul>
+        {records.map((record, index) => (
+          <li key={index}>{record.title} {record.time}時間</li>
+        ))}
+      </ul>
     </>
   )
 }
